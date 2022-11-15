ecb4a8b052014bde3f363da290881036cada020e
ThallesKraft <99352499+ThallesKraft009@users.noreply.github.com>
2022-11-15 06:58:25 -0300

    Create message.js

---
+++ message.js
@@ -0,0 +1,13 @@
+module.exports = {
+  giveaway: "<a:bop:788210473454796861> Novo sorteio iniciado!",
+    giveawayEnded: "<:Sad_cat3:757603353311182898> Sorteio encerrado!",
+    inviteToParticipate: "Reaja a 🎉 para participar!",
+    dropMessage: "Seja o primeiro a reagir com 🎉 !",
+    drawing: 'Tempo: {timestamp}',
+    winMessage: "Parabéns aos ganhadores por ganhar o {this.prize}!\n> ⚡ | {winners}!",
+    embedFooter: "Sorteio :D",
+    noWinner: "Sorteio cancelado, sem participações.",
+    hostedBy: "Hospedado por: {this.hostedBy}",
+    winners: "Ganhador(s)",
+    endedAt: "Terminado em"
+}
